49b6176bd307f762958a52bd59b32635f4c9d8d8
Tamara <tamara.northe@laparkan.com>
2023-12-17 21:01:43 -0400

    feed/sidebar styling completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import {Sidebar,Videos} from './';
 
 const Feed = () => {
   return (
@@ -10,6 +10,13 @@ const Feed = () => {
         <Typography className='copyright' variant='body2' sx={{mt:1.5, color: '#fff'}}>
           Copyright 2023 Tamara Inniss
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant="h4" fontWeight="bold" mb={2} 
+          sx={{ color: 'white'}}>
+          New<span style={{ color: '#F31503'}}>videos</span>
+        </Typography>
+        <Videos/>
       </Box>
     </Stack>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
 
+const selectedCategory = 'New';
+
 const   Sidebar = () => (
     <Stack 
         direction="row"
@@ -11,9 +13,21 @@ const   Sidebar = () => (
 
         }}>
             {categories.map((category) => (
-                <button>
-                    <span>{category.icon}</span>
-                    <span>{category.name}</span>
+                <button 
+                    className="category-btn"
+                    style={{
+                        background: category.name ===
+                        selectedCategory && '#FC1503',
+                        color: 'white'
+                    }}
+                    key={category.name}
+                    >
+                    <span style={{color: category.name ===
+                    selectedCategory ? 'white' : 'red', marginRight: '15px'}}>
+                        {category.icon}</span>
+                    <span style={{ opacity: category.name ===
+                    selectedCategory ? '1': '0.8'}}>
+                        {category.name}</span>
                 </button>
             ))}
     </Stack>    
