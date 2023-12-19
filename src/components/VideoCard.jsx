343eaebbd8f6297bab5b37e2468f229d8de79d42
Tamara <tamara.northe@laparkan.com>
2023-12-19 16:41:05 -0400

    started creating layout for how each video will look when displayed

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
+
+
+const VideoCard = ({video}) => {
+  console.log(video);
+  return (
+    <div>
+      VIdeoCard
+    </div>
+  )
+}
+
+export default VideoCard
