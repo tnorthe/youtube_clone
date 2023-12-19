5a7d743d27fd0124026da43dcc030736634cfaee
Tamara <tamara.northe@laparkan.com>
2023-12-19 17:25:06 -0400

    rendered video details from api to screen and made some styling changes to videoCard component

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,12 +5,25 @@ import { CheckCircle } from "@mui/icons-material";
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
 
-const VideoCard = ({video}) => {
-  console.log(video);
+const VideoCard = ({video: {id:{videoId}, snippet}}) => {
+  /*console.log(videoId, snippet);*/
   return (
-    <div>
-      VIdeoCard
-    </div>
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia 
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{width:358, height: 180}}
+        />
+        <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px'}}>
+          <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+            <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+              {snippet?.title.slice(0,60) || demoVideoTitle.slice(0,60)}
+            </Typography>
+          </Link>
+        </CardContent>
+      </Link>
+    </Card>
   )
 }
 
