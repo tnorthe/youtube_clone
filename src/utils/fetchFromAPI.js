cdf63a35d58fa7a10fcfac6739f9775891bfcf8b
Tamara <tamara.northe@laparkan.com>
2023-12-19 14:29:13 -0400

    finished connecting to api

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions';
 
 const options = {
-    url: BASE_URL,
     params: {
       part: 'snippet',
       videoId: 'M7FIvfx5J10'
@@ -13,3 +12,9 @@ const options = {
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
+
+  export const fetchFromAPI = async (url) => {
+    const {data} = await axios.get(`${BASE_URL}/${url}`, options);
+
+    return data;
+  }
