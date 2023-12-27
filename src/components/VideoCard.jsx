81f3f6c98e01ec4617b82f01a3a6ad2fc590a2ab
Tamara <tamara.northe@laparkan.com>
2023-12-27 15:43:30 -0400

    minor changes made to improve responsive layout

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,12 +8,12 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 const VideoCard = ({video: {id:{videoId}, snippet}}) => {
   /*console.log(videoId, snippet);*/
   return (
-    <Card sx={{width: {md: '320px', xs:'100%'}, boxShadow: 'none', borderRadius: 'none'}}>
+    <Card sx={{width: {xs:'100%',sm:'358px',md: '320px'}, boxShadow: 'none', borderRadius: 'none'}}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia 
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{width:358, height: 180}}
+          sx={{width:{xs:'100%', sm:'358px', md:'320px'}, height: 180}}
         />
         <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px'}}>
           <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
