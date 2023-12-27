c690c310396b6a21cae9979a8599d961c0b14724
Tamara <tamara.northe@laparkan.com>
2023-12-27 14:27:37 -0400

    corrected some code

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({channelDetail}) => (
+const ChannelCard = ({channelDetail, marginTop}) => (
     <Box 
       sx={{
         boxShadow: 'none', 
@@ -14,7 +14,8 @@ const ChannelCard = ({channelDetail}) => (
         alignItems: 'center',
         width: {xs: '356px', md: '320px'},
         height: '326px',
-        margin: 'auto'
+        margin: 'auto',
+        marginTop
         }}> 
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
         <CardContent sx={{display: 'flex', flexDirection: 'column', 
@@ -31,7 +32,7 @@ const ChannelCard = ({channelDetail}) => (
             </Typography>
             {channelDetail?.statistics?.subscriberCount && (
               <Typography>
-                {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString} Subscribers
+                {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
               </Typography>
             )}
         </CardContent>
