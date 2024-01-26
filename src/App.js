0b2f5d617ee9a5e3a2e3c4e0ff16529bcae25b41
Tamara <tamara.northe@laparkan.com>
2024-01-26 15:33:03 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Box } from "@mui/material";
 import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from "./components";
 
 const App = () =>  (
-    <BrowserRouter>
+    <BrowserRouter basename="/youtube_clone">
         <Box sx={{backgroundColor: '#000'}}>
             <Navbar />
             <Routes>
-                <Route path="/youtube_clone" exact element={<Feed />}/>
+                <Route path="/" exact element={<Feed />}/>
                 <Route path="/video/:id" element={<VideoDetail />}/>
                 <Route path="/channel/:id" element={<ChannelDetail/>}/>
                 <Route path="/search/:searchTerm" element={<SearchFeed/>}/>
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -20,7 +20,7 @@ const Feed = () => {
           setSelectedCategory={setSelectedCategory}
         />
         <Typography className='copyright' variant='body2' sx={{mt:1.5, color: '#fff'}}>
-          Copyright 2023 Tamara Inniss
+          Copyright 2023 Tamara Northe
         </Typography>
       </Box>
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
